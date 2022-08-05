6d64f3154f29525982a33b24640b185a2ee571dd
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-05 15:44:58 +0900

    교차 검증과 그리드 서치

--- src/chap5/CrossValidate.py
+++ src/chap5/CrossValidate.py
@@ -1,10 +1,10 @@
+"""# 교차 검증과 그리드 서치"""
+
+"""## 검증 세트"""
+
 import pandas as pd
 
 wine = pd.read_csv('https://bit.ly/wine_csv_data')
-wine.head()
-
-wine.info()
-wine.describe()
 
 data = wine[['alcohol', 'sugar', 'pH']].to_numpy()
 target = wine['class'].to_numpy()
@@ -14,66 +14,99 @@ from sklearn.model_selection import train_test_split
 train_input, test_input, train_target, test_target = train_test_split(
     data, target, test_size=0.2, random_state=42)
 
-print(train_input.shape, test_input.shape)
+sub_input, val_input, sub_target, val_target = train_test_split(
+    train_input, train_target, test_size=0.2, random_state=42)
 
-from sklearn.preprocessing import StandardScaler
+print(sub_input.shape, val_input.shape)
 
-ss = StandardScaler()
-ss.fit(train_input)
-
-train_scaled = ss.transform(train_input)
-test_scaled = ss.transform(test_input)
-
-from sklearn.linear_model import LogisticRegression
-
-lr = LogisticRegression()
-lr.fit(train_scaled, train_target)
-
-print(lr.score(train_scaled, train_target))
-print(lr.score(test_scaled, test_target))
-
-print(lr.coef_, lr.intercept_)
-
-
-# 결정 트리
 from sklearn.tree import DecisionTreeClassifier
 
 dt = DecisionTreeClassifier(random_state=42)
-dt.fit(train_scaled, train_target)
+dt.fit(sub_input, sub_target)
 
-print(dt.score(train_scaled, train_target))
-print(dt.score(test_scaled, test_target))
+print(dt.score(sub_input, sub_target))
+print(dt.score(val_input, val_target))
 
-import matplotlib.pyplot as plt
-from sklearn.tree import plot_tree
+"""## 교차 검증"""
 
-plt.figure(figsize=(10,7))
-plot_tree(dt)
-plt.show()
+from sklearn.model_selection import cross_validate
 
-plt.figure(figsize=(10,7))
-plot_tree(dt, max_depth=1, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
-plt.show()
+scores = cross_validate(dt, train_input, train_target)
+print(scores)
 
-# 가지치기
-dt = DecisionTreeClassifier(max_depth=3, random_state=42)
-dt.fit(train_scaled, train_target)
+import numpy as np
 
-print(dt.score(train_scaled, train_target))
-print(dt.score(test_scaled, test_target))
+print(np.mean(scores['test_score']))
 
-plt.figure(figsize=(20,15))
-plot_tree(dt, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
-plt.show()
+from sklearn.model_selection import StratifiedKFold
 
-dt = DecisionTreeClassifier(max_depth=3, random_state=42)
-dt.fit(train_input, train_target)
+scores = cross_validate(dt, train_input, train_target, cv=StratifiedKFold())
+print(np.mean(scores['test_score']))
 
+splitter = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+scores = cross_validate(dt, train_input, train_target, cv=splitter)
+print(np.mean(scores['test_score']))
+
+"""## 하이퍼파라미터"""
+
+from sklearn.model_selection import GridSearchCV
+
+params = {'min_impurity_decrease': [0.0001, 0.0002, 0.0003, 0.0004, 0.0005]}
+
+gs = GridSearchCV(DecisionTreeClassifier(random_state=42), params, n_jobs=-1)
+
+gs.fit(train_input, train_target)
+
+dt = gs.best_estimator_
 print(dt.score(train_input, train_target))
+
+print(gs.best_params_)
+
+print(gs.cv_results_['mean_test_score'])
+
+best_index = np.argmax(gs.cv_results_['mean_test_score'])
+print(gs.cv_results_['params'][best_index])
+
+params = {'min_impurity_decrease': np.arange(0.0001, 0.001, 0.0001),
+          'max_depth': range(5, 20, 1),
+          'min_samples_split': range(2, 100, 10)
+          }
+
+gs = GridSearchCV(DecisionTreeClassifier(random_state=42), params, n_jobs=-1)
+gs.fit(train_input, train_target)
+
+print(gs.best_params_)
+
+print(np.max(gs.cv_results_['mean_test_score']))
+
+"""## 랜덤 서치"""
+
+from scipy.stats import uniform, randint
+
+rgen = randint(0, 10)
+rgen.rvs(10)
+
+np.unique(rgen.rvs(1000), return_counts=True)
+
+ugen = uniform(0, 1)
+ugen.rvs(10)
+
+params = {'min_impurity_decrease': uniform(0.0001, 0.001),
+          'max_depth': randint(20, 50),
+          'min_samples_split': randint(2, 25),
+          'min_samples_leaf': randint(1, 25),
+          }
+
+from sklearn.model_selection import RandomizedSearchCV
+
+gs = RandomizedSearchCV(DecisionTreeClassifier(random_state=42), params,
+                        n_iter=100, n_jobs=-1, random_state=42)
+gs.fit(train_input, train_target)
+
+print(gs.best_params_)
+
+print(np.max(gs.cv_results_['mean_test_score']))
+
+dt = gs.best_estimator_
+
 print(dt.score(test_input, test_target))
-
-plt.figure(figsize=(20,15))
-plot_tree(dt, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
-plt.show()
-
-print(dt.feature_importances_)
